19cc2694399825d182feb08af8c1863c9eb46fd7
jimmy <jimmycheong7@gmail.com>
2017-05-25 15:33:57 +0100

    Progress 1

--- Lesson 3 challenge/delegate2.swift
+++ Lesson 3 challenge/delegate2.swift
@@ -10,17 +10,43 @@ import Foundation
 import UIKit
 
 class cashFieldDelegate : NSObject, UITextFieldDelegate {
+
+    var pennies: String = ""
     
-    override init(){
-        var pennies : String = "lemons"
+    override init () {
+        
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
-//        var newString: String
-//        newString = "$" + (textField.text)!
-        textField.text = self.pennies
-        return true
         
+        
+        pennies = (textField.text)!
+        var newText = pennies as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        if pennies == "$0.00" {
+        
+            print("nexttext.length: ")
+            
+            let index = pennies.index(pennies.startIndex, offsetBy: newText.length - 1)
+            pennies = pennies.substring(to: index)
+            print("It works!")
+            print("Pennies: " + pennies)
+            
+            textField.text = pennies
+        
+        } else {
+        
+            print("nothing")
+        }
+        
+        //
+//        let index = pennies.index(pennies.startIndex, offsetBy: 1)
+//        let result = pennies.substring(from: index)
+//        
+//        textField.text = "$" + result
+//        return true
+     
+        return true
     }
 }
